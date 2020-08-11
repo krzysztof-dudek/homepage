ba47aaf8ab5ec6daebd19ac6902d92c51b536912
Krzysztof Dudek <krzysiud123@gmail.com>
2020-08-11 17:11:44 +0200

    named by BEM

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
-const name = 'Krzysztof';
+const name = "Krzysztof";
 const age = 14;
 
-alert ('Cześć')
+alert("Cześć");
 
 console.log(name);
 console.log(age);
-console.log(`nazywam się ${name} i mam ${age} lat.`);
+console.log(`Nazywam się ${name} i mam ${age} lat.`
+);
+
+const heading = document.querySelector('.main__heading--js');
+
+heading. innerHTML = `Nazywam się ${name} i mam ${age} lat.`
+
+
+
